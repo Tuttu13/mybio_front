a6175a69f45f82671e793ec429cd42dbe5079529
Tuttu13 <tutui0426@yahoo.co.jp>
2024-11-16 17:54:04 +0900

    仮デザイン

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,14 @@
-import {createTheme,ThemeProvider} from '@mui/material/styles';
-import About from './components/About';
-// import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Hobbies from './components/Hobbies';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import PersonalProjects from './components/PersonalProjects';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#007BFF',
-    },
-    secondary: {
-      main: '#66D4D4',
-    },
-  },
-  typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    h3: {
-      fontSize: '2.5rem',
-      fontWeight: 700,
-      marginBottom: '1rem',
-    },
-    body1: {
-      fontSize: '1.125rem',
-      lineHeight: 1.6,
-    },
-    button: {
-      textTransform: 'none',
-    },
-  },
-});
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-      <div style={{ marginTop: '64px' }}>
-        <Home />
-        <About />
-        <Skills />
-        <Projects />
-        <PersonalProjects />
-        <Hobbies />
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   );
 }
 
