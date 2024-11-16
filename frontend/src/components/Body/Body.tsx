b64f02770c9a65b39f0efe17dcc407c567c48d68
Tuttu13 <tutui0426@yahoo.co.jp>
2024-11-16 18:28:19 +0900

    デザイン変更

--- frontend/src/components/Body/Body.tsx
+++ frontend/src/components/Body/Body.tsx
@@ -1,54 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Body: React.FC = () => {
   return (
     <>
-      <div className="artboard artboard-horizontal phone-6">
-        <div className="">1024×320</div>
-      </div>
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            alt=""
-            className="w-full"
-          />
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+        }}
+      >
+        {/* <div className="hero-overlay bg-opacity-60"></div> */}
+        <div className="text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello thereaaaaaa</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
-        <div id="item2" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            alt=""
-            className="w-full"
-          />
-        </div>
-        <div id="item3" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            alt=""
-            className="w-full"
-          />
-        </div>
-        <div id="item4" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            alt=""
-            className="w-full"
-          />
-        </div>
-      </div>
-      <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-        <a href="#item4" className="btn btn-xs">
-          4
-        </a>
       </div>
     </>
   );
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,39 +1,62 @@
-import React from 'react';
+import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <div className="navbar bg-base-300 w-full">
-      <div className="flex-1 px-2">Navbar Title</div>
-      {/* ドロワーボタン（小画面用） */}
-      <div className="flex-none lg:hidden">
+    <div className="drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="navbar bg-base-300 w-full">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="mx-2 flex-1 px-2">Navbar Title</div>
+          <div className="hidden flex-none lg:block">
+            <ul className="menu menu-horizontal">
+              {/* Navbar menu content here */}
+              <li>
+                <a href="/">Navbar Item 1</a>
+              </li>
+              <li>
+                <a href="/">Navbar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Page content here */}
+        Content
+      </div>
+      <div className="drawer-side">
         <label
           htmlFor="my-drawer-3"
-          aria-label="open sidebar"
-          className="btn btn-square btn-ghost"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-6 w-6 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </label>
-      </div>
-      {/* メニューと大画面用ドロワーボタン */}
-      <div className="hidden flex-none lg:flex lg:justify-end">
-        <ul className="menu menu-horizontal">
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+          {/* Sidebar content here */}
           <li>
-            <a href="/">Navbar Item 1</a>
+            <a href="/">Sidebar Item 1</a>
           </li>
           <li>
-            <a href="/">Navbar Item 2</a>
+            <a href="/">Sidebar Item 2</a>
           </li>
         </ul>
       </div>
