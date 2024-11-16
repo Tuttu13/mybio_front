e4390f46b715c8000b810d024cf7dfdd49388bdb
Tuttu13 <tutui0426@yahoo.co.jp>
2024-11-16 17:54:22 +0900

    deisyUIテーマ追加

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    themes: ["light", "dark", "cupcake"],
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")]
 }
-
